e058ff7477c6c45d0986b25bab5da5987563fb60
kenzo2-tech <kenzieg@nycstudents.net>
2026-02-06 04:32:42 -0500

    Update script.js

--- Final/script.js
+++ Final/script.js
@@ -5,3 +5,11 @@ const rockButton = document.querySelector('#rock')
 const rnbButton = document.querySelector('#rnb')
 const jazzButton = document.querySelector('#jazz')
 const neoButton = document.querySelector('#neo-soul')
+
+const brentIMG = document.querySelector('#brent')
+
+const rnbGen = () => {
+    brentIMG.classList.toggle('hidden')
+}
+
+rnbButton.addEventListener('click',rnbGen)
