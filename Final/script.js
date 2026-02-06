bcbb1c93c3a61095b49ad59aeb6c7c916e000e76
kenzo2-tech <kenzieg@nycstudents.net>
2026-02-06 04:24:47 -0500

    Add files via upload

---
+++ Final/script.js
@@ -0,0 +1,7 @@
+console.log('hey')
+
+const rapButton = document.querySelector('#rap')
+const rockButton = document.querySelector('#rock')
+const rnbButton = document.querySelector('#rnb')
+const jazzButton = document.querySelector('#jazz')
+const neoButton = document.querySelector('#neo-soul')
